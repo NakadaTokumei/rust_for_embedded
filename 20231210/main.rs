396e4754ae5b24c59fbad22049e36b3dd5cb83d3
HeisenBug <shk052353@gmail.com>
2023-12-11 01:05:39 +0900

    [Add] Working on baremental firmware 20231211

--- 20231210/main.rs
+++ 20231210/main.rs
@@ -1,7 +1,27 @@
 use nopanic;
 
 #[no_mangle]
-pub extern "C" fn main() -> !
+pub unsafe extern "C" fn Reset_Handler()
+{
+    main();
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn Loop_Handler()
+{
+    loop {}
+}
+
+#[no_mangle]
+#[link_section = ".isr_vector"]
+pub static RESET_VECTOR: [unsafe extern "C" fn(); 2] = 
+[
+    Reset_Handler,
+    Loop_Handler
+];
+
+#[no_mangle]
+pub unsafe extern "C" fn main() -> !
 {
     loop {}
 }
